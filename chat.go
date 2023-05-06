e4740ecff52fbfbb14d225863d65dd79b797474f
Illya Busigin <illya@cyrillian.com>
2023-05-06 16:54:26 -0500

    Remove omitempty from ChatCompletionRequest

--- chat.go
+++ chat.go
@@ -33,14 +33,14 @@ type ChatCompletionMessage struct {
 type ChatCompletionRequest struct {
 	Model            string                  `json:"model"`
 	Messages         []ChatCompletionMessage `json:"messages"`
-	MaxTokens        int                     `json:"max_tokens,omitempty"`
-	Temperature      float32                 `json:"temperature,omitempty"`
-	TopP             float32                 `json:"top_p,omitempty"`
-	N                int                     `json:"n,omitempty"`
-	Stream           bool                    `json:"stream,omitempty"`
-	Stop             []string                `json:"stop,omitempty"`
-	PresencePenalty  float32                 `json:"presence_penalty,omitempty"`
-	FrequencyPenalty float32                 `json:"frequency_penalty,omitempty"`
+	MaxTokens        int                     `json:"max_tokens"`
+	Temperature      float32                 `json:"temperature"`
+	TopP             float32                 `json:"top_p"`
+	N                int                     `json:"n"`
+	Stream           bool                    `json:"stream"`
+	Stop             []string                `json:"stop"`
+	PresencePenalty  float32                 `json:"presence_penalty"`
+	FrequencyPenalty float32                 `json:"frequency_penalty"`
 	LogitBias        map[string]int          `json:"logit_bias,omitempty"`
 	User             string                  `json:"user,omitempty"`
 }
